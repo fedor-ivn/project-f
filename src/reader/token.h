26375623ab8c12df260386f9c860b8cddbd146bd
Ilnar Khasanov <4sitescarp@gmail.com>
2023-09-09 14:16:48 +0300

    Add .gitignore Add classes of tokens

--- src/reader/token.h
+++ src/reader/token.h
@@ -3,3 +3,9 @@
 class Token {};
 
 class LeftParenthesis : Token {};
+class RightParenthesis : Token {};
+class Identifier : Token {};
+class Integer : Token {};
+class Real : Token {};
+class True : Token {};
+class False : Token {};
