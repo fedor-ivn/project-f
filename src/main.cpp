f94b033a7001f63bb76c4d153694678b01e485e1
SnejUgal <contact@snejugal.ru>
2023-09-17 19:36:46 +0300

    Read source code from stdin

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,18 @@
 #include "reader/token.h"
 
 int main() {
-    std::string_view source(
-        "(func ; a\nfoo ; b\n(1 ; c\n2.0 'kek null -3)\ntrue) ; ; ; ; ...");
-    Scanner scanner(source);
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        std::string_view source(line);
+        Scanner scanner(source);
 
-    while (true) {
-        std::unique_ptr<Token> token = scanner.next_token();
-        if (EndOfFile* new_token = dynamic_cast<EndOfFile*>(&*token)) {
-            break;
+        while (true) {
+            std::unique_ptr<Token> token = scanner.next_token();
+            if (EndOfFile* new_token = dynamic_cast<EndOfFile*>(&*token)) {
+                break;
+            }
+            std::cout << *token << std::endl;
         }
-        std::cout << *token << std::endl;
     }
 
     return 0;
