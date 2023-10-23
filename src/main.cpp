8f32b66b20299c713b338b0bf52d6d8e1be9b382
Fedor Ivanov <ivnfedor@gmail.com>
2023-10-23 21:45:36 +0300

    Just ignore recoverable errors

--- src/main.cpp
+++ src/main.cpp
@@ -175,7 +175,7 @@ void process(
     Evaluator& evaluator,
     std::string_view source,
     Position position,
-    bool preserve_recoverable = false
+    bool ignore_recoverable = false
 ) {
     try {
         Scanner scanner(source, position);
@@ -202,8 +202,8 @@ void process(
             std::cout << output->display_pretty() << std::endl;
         }
     } catch (SyntaxError const& error) {
-        if (preserve_recoverable && error.can_recover) {
-            throw error;
+        if (ignore_recoverable && error.can_recover) {
+            return;
         }
         std::cerr << error << std::endl;
     }
@@ -229,19 +229,16 @@ void repl(Mode mode) {
 
         buffered.append(line).push_back('\n');
         ++lines_buffered;
-        try {
-            process(
-                mode,
-                evaluator,
-                std::string_view(buffered),
-                Position(lines_executed + 1),
-                true
-            );
-            buffered = "";
-            lines_executed = current_line;
-            lines_buffered = 0;
-        } catch (SyntaxError const& error) {
-        }
+        process(
+            mode,
+            evaluator,
+            std::string_view(buffered),
+            Position(lines_executed + 1),
+            true
+        );
+        buffered = "";
+        lines_executed = current_line;
+        lines_buffered = 0;
     }
 }
 
