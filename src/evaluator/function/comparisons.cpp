45c45043f78ebb8e433aa68085f3cdd4732c0a93
4sitescarp@gmail.com <4sitescarp@gmail.com>
2023-11-19 01:08:39 +0300

    Evaluate greatereq

--- src/evaluator/function/comparisons.cpp
+++ src/evaluator/function/comparisons.cpp
@@ -254,23 +254,23 @@ std::shared_ptr<Element> GreatereqFunction::call(CallFrame frame) const {
     auto a_element = frame.arguments[0];
     auto b_element = frame.arguments[1];
 
-    if (a_element->kind != b_element->kind) {
-        throw EvaluationError(
-            "`greatereq` expects that arguments have the same type",
-            frame.call_site
-        );
-    }
-
     bool result;
 
-    if (auto a_real = std::dynamic_pointer_cast<ast::Real>(a_element)) {
+    if (a_element->kind == ElementKind::REAL &&
+        b_element->kind == ElementKind::REAL) {
+        auto a_real = std::dynamic_pointer_cast<ast::Real>(a_element);
         auto b_real = std::dynamic_pointer_cast<ast::Real>(b_element);
+
         result = a_real->value >= b_real->value;
-    } else if (auto a_int = std::dynamic_pointer_cast<ast::Integer>(a_element)) {
+    } else if (a_element->kind == ElementKind::INTEGER && b_element->kind == ElementKind::INTEGER) {
+        auto a_int = std::dynamic_pointer_cast<ast::Integer>(a_element);
         auto b_int = std::dynamic_pointer_cast<ast::Integer>(b_element);
+
         result = a_int->value >= b_int->value;
-    } else if (auto a_bool = std::dynamic_pointer_cast<ast::Boolean>(a_element)) {
+    } else if (a_element->kind == ElementKind::BOOLEAN && b_element->kind == ElementKind::BOOLEAN) {
+        auto a_bool = std::dynamic_pointer_cast<ast::Boolean>(a_element);
         auto b_bool = std::dynamic_pointer_cast<ast::Boolean>(b_element);
+
         result = a_bool->value >= b_bool->value;
     } else {
         throw EvaluationError(
